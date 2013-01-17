f35727bc0728d67447ea3b74d32b103594ab1aca
Jun <gojun077@gmail.com>
2013-01-17 20:35:31 +0900

    Once you decode the message, it tells you to apply the Caesar shift on the URL, which gives you a hint for the location of problem 2. It also recommends that the method string.translate() and string.maketrans() be used for decoding the shifted message.

--- pythonChallenge1.py
+++ pythonChallenge1.py
@@ -3,11 +3,12 @@
 # coding = UTF-8
 import string
 
-cstring = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq \
+'''cstring = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq \
 ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr \
 gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc \
-spj."
+spj."'''
 
+cstring = "http://www.pythonchallenge.com/pc/def/map.html"
 cList = []
 # convert string to list
 for char in cstring:
